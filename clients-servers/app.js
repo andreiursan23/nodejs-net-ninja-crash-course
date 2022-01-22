860511fc2b289e6dd1ac9d12b2eb15766cefae22
andreiursan23 <ursanandrei@ymail.com>
2022-01-22 12:20:24 +0200

    More progress

--- clients-servers/app.js
+++ clients-servers/app.js
@@ -9,12 +9,25 @@ app.set("view engine", "ejs");
 // listen for req
 app.listen(3000);
 
+app.use((req, res) => {
+  console.log("new req made");
+  console.log("host: ", req.hostname);
+  console.log("path: ", req.path);
+  console.log("method: ", req.method);
+  console.log("---------------------");
+})
+
 app.get("/", (req, res) => {
   // this method sets up the req header automatically
   // res.send("<p>Home page</p>");
   // res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {title: "Phenomenal programmer learns back-end", snippet:"Lorem ipsum dolor sit amet consectetur"},
+    {title: "Front-end interviews", snippet:"Lorem ipsum dolor sit amet consectetur"},
+    {title: "NextJS is the next big thing", snippet:"Lorem ipsum dolor sit amet consectetur"},
+  ]
 
-  res.render("index", { title: "Home" });
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
